e10495145b18fb46abde9127937b4155d47197a1
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2017-02-16 06:23:16 -0500

    Add examples section

---
+++ pages/examples/_template.jsx
@@ -0,0 +1,3 @@
+import OtherTemplate from '../docs/_template';
+
+export default OtherTemplate;
---
+++ pages/examples/asList/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+exports.data = {
+  title: 'As a list of "cards"',
+  order: 6
+}
+
+export default class extends Component {
+  render() {
+    return <h1>Hi!</h1>
+  }
+}
