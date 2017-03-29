09a7d58113b9d5acfb47bd893451f6d981d06f54
alexbaumgertner <alex.baumgertner@gmail.com>
2017-03-30 00:16:10 +0300

    Add getting row data code example

--- pages/examples/getDataFromRowIntoCell/index.js
+++ pages/examples/getDataFromRowIntoCell/index.js
@@ -13,6 +13,52 @@ exports.data = {
 
 const intro = `
 Griddle supports getting data from Row into Cell.
+
+First we need two helpers: row data selector and wrapper:
+\`\`\`js
+import { connect } from 'react-redux';
+
+const rowDataSelector = (state, { griddleKey }) => {
+  return state
+    .get('data')
+    .find(rowMap => rowMap.get('griddleKey') === griddleKey)
+    .toJSON();
+};
+
+const enhancedWithRowData = connect((state, props) => {
+  return {
+    // rowData will be available into MyCustomComponent
+    rowData: rowDataSelector(state, props)
+  };
+});
+\`\`\`
+
+MyCustomComponent:
+
+\`\`\`js
+function MyCustomComponent({ value, griddleKey, rowData }) {
+  // rowData
+}
+\`\`\`
+
+With helpers declared above ColumnDefinition customComponent has access to Row data:
+
+\`\`\`js
+return (
+  <Griddle
+    data={yourData}
+    plugins={[plugins.LocalPlugin]}
+  >
+    <RowDefinition>
+      <ColumnDefinition
+        id="someKey"
+        title="My Title"
+        customComponent={enhancedWithRowData(MyCustomComponent)}
+      />
+    </RowDefinition>
+  </Griddle>
+);
+\`\`\`
 `
 
 export default class extends Component {
