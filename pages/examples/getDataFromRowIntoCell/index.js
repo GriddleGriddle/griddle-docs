f04effa8499f7706aea41018d01ae9135674ecfc
alexbaumgertner <alex.baumgertner@gmail.com>
2017-03-30 09:26:23 +0300

    Add real example

--- pages/examples/getDataFromRowIntoCell/index.js
+++ pages/examples/getDataFromRowIntoCell/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import { config } from 'config';
 import DocumentTitle from 'react-document-title';
 import Markdown from 'components/Markdown';
 
 import Griddle, { plugins, RowDefinition, ColumnDefinition } from 'griddle-react';
+import fakeData from 'utils/fakeData';
 
 exports.data = {
   title: 'Get data from Row into Cell',
@@ -12,9 +13,9 @@ exports.data = {
 }
 
 const intro = `
-Griddle supports getting data from Row into Cell.
+#### Griddle supports getting data from Row into Cell.
 
-First we need two helpers: row data selector and wrapper:
+First we need two helpers: row data selector and connect-wrapper:
 \`\`\`js
 import { connect } from 'react-redux';
 
@@ -37,42 +38,100 @@ MyCustomComponent:
 
 \`\`\`js
 function MyCustomComponent({ value, griddleKey, rowData }) {
-  // rowData
+  return (
+    <div className="MyCustomComponent">
+      Person <strong>{value}</strong> from city: <em>{rowData.city}</em>
+    </div>
+  );
 }
 \`\`\`
 
-With helpers declared above ColumnDefinition customComponent has access to Row data:
+With enhancedWithRowData helper customComponent will have access to Row data:
 
 \`\`\`js
-return (
-  <Griddle
-    data={yourData}
-    plugins={[plugins.LocalPlugin]}
+const data = [
+  {
+    "name": "Mayer Leonard",
+    "city": "Kapowsin"
+  },
+  {
+    "name": "Koch Becker",
+    "city": "Johnsonburg"
+  }
+];
+
+<Griddle
+  data={data}
+  plugins={[plugins.LocalPlugin]}
+  components={{
+    Layout: ({ Table }) => <Table />
+  }}
+  pageProperties={{
+    pageSize: 2
+  }}
   >
-    <RowDefinition>
-      <ColumnDefinition
-        id="someKey"
-        title="My Title"
-        customComponent={enhancedWithRowData(MyCustomComponent)}
-      />
-    </RowDefinition>
-  </Griddle>
-);
+  <RowDefinition>
+    <ColumnDefinition
+      id="name"
+      title="Persons"
+      customComponent={enhancedWithRowData(MyCustomComponent)}
+    />
+  </RowDefinition>
+</Griddle>
 \`\`\`
 `
+
+const rowDataSelector = (state, { griddleKey }) => {
+  return state
+    .get('data')
+    .find(rowMap => rowMap.get('griddleKey') === griddleKey)
+    .toJSON();
+};
+
+const enhancedWithRowData = connect((state, props) => {
+  return {
+    rowData: rowDataSelector(state, props)
+  };
+});
+
+function MyCustomComponent({ value, griddleKey, rowData }) {
+  return (
+    <div className="MyCustomComponent">
+      Person <strong>{value}</strong> from city: <em>{rowData.city}</em>
+    </div>
+  );
+}
 
 export default class extends Component {
   render() {
     const page = this.props.route.page;
 
-    const sortProperties = [
-      { id: 'name', sortAscending: true }
-    ];
-
     return (
       <DocumentTitle title={`${page.data.title} | ${config.siteTitle}`}>
         <div>
           <Markdown text={intro} />
+
+          <hr/>
+          <p>Example:</p>
+
+          <Griddle
+            data={fakeData}
+            plugins={[plugins.LocalPlugin]}
+            components={{
+              Layout: ({ Table }) => <Table />
+            }}
+            pageProperties={{
+              pageSize: 2
+            }}
+          >
+            <RowDefinition>
+              <ColumnDefinition
+                id="name"
+                title="Persons"
+                customComponent={enhancedWithRowData(MyCustomComponent)}
+              />
+            </RowDefinition>
+          </Griddle>
         </div>
       </DocumentTitle>
     );
